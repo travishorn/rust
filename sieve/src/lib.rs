ed95e479e4992af6efccd59960c67de9bf6cf398
Travis Horn <travis@travishorn.com>
2023-03-03 12:12:08 -0600

    Optimize Sieve

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,55 +1,59 @@
-/// Get the multiples of a given number up to a given upper bound
-fn get_multiples(n: u64, upper_bound: u64) -> Vec<u64> {
-    // Initialize a new vector
-    let mut result = Vec::new();
-
-    // Start with the given number multiplied by 2.
-    // We dont start with the number itself because we don't want to consider
-    // the number itself a multiple for the purposes of this exercise
-    let mut current = n * 2;
-
-    // Start a loop that continues as long as the current number is less than or equal to the given
-    // upper bound
-    while current <= upper_bound {
-        // Push the current number into the vector
-        result.push(current);
-
-        // Add the given number to the current number to get the next multiple
-        current += n;
-    }
-
-    // Return the resulting vector which contains all multiples
-    result
-}
-
-/// Get the primes up to a given upper bound
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    // Store a vector of all numbers from 2 up to the upper bound
-    let mut ns: Vec<u64> = (2..=upper_bound).collect();
+    // Create a vector of booleans up the the upper bound + 1. All values are initially true
+    let mut is_prime = vec![true; (upper_bound + 1) as usize];
 
-    // Initialize an index variable to keep track of where we are in the vector
-    let mut i = 0;
+    // Mark 0 as not prime
+    is_prime[0] = false;
 
-    // Keep looping as long as we haven't reached the end of the vector
-    // We cannot use `for n in ns` here because we're modifying ns
-    // We cannot use `for i in 0..ns.len()` because we're taking elements out of ns, making it
-    // shorter. Eventually i will be an index out-of-bounds of the shortened ns vector
-    while i < ns.len() {
-        // Store the value at the index
-        let n = ns[i];
-
-        // Get the multiples of that number (except itself)
-        let multiples = get_multiples(n, upper_bound);
-
-        // Modify ns, keeping only the values that do not exist in the multiples vector
-        // Effectively, removing all multiples
-        ns.retain(|x| !multiples.contains(x));
-
-        // Increment the index for the next iteration
-        i += 1;
+    // If the upper bound is at least 1, mark 1 not prime
+    if upper_bound >= 1 {
+        is_prime[1] = false;
     }
 
-    // Return the resulting vector, which has all numbers that are multiples of other numbers
-    // removed. Meaning it only contains prime numbers.
-    ns
+    // Initialize a new `current` number, starting at the first prime number, 2
+    let mut current = 2;
+
+    // Start a loop that continues as long as the square of the current number is less than or equal
+    // to the upper bound
+    while current * current <= upper_bound {
+        // If the current number is prime...
+        if is_prime[current as usize] {
+            // Start marking its multiples as not prime
+
+            // Initialize the first multiple to check. We start with the current number squared
+            // because we already know all previous multiples have already been removed in previous
+            // iterations of the loop
+            let mut multiple = current * current;
+
+            // While the multiple in in bounds...
+            while multiple <= upper_bound {
+                // Set the value that represents it in the vector to false, marking it not prime
+                is_prime[multiple as usize] = false;
+
+                // Go to the next multiple
+                multiple += current;
+            }
+        }
+
+        // Go to the next number in the vector
+        current += 1;
+    }
+
+    // We are left with a vector full of booleans where true = prime and false = not prime
+    // We need to turn this into a vector of numbers
+
+    // Initialize a new vector
+    let mut primes = Vec::new();
+
+    // Iterate over all numbers from 2 to the upper bound
+    for n in 2..=upper_bound {
+        // If the boolean representation of this number in our is_primes vector is true...
+        if is_prime[n as usize] {
+            // Push the number into the newly created primes vector
+            primes.push(n);
+        }
+    }
+
+    // Return the vector of primes
+    primes
 }
