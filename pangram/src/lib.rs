6be56d5eb66d763716d0226482df23fb7493e530
Travis Horn <travis@travishorn.com>
2023-03-03 10:22:18 -0600

    Optimize Pangram

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,24 +1,27 @@
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    // A string of every lowercase letter
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    sentence
+        // Convert the given sentence to lowercase
+        .to_lowercase()
+        // Split the sentence into an iterator over its characters
+        .chars()
+        // Filter so that only lowercase ASCII letters remain
+        .filter(|c| c.is_ascii_lowercase())
+        // Fold the iterator into an array
+        // The initial array is 26 instances of `false` (one for each letter of the alphabet)
+        .fold([false; 26], |mut acc, c| {
+            // Convert the letter into its ASCII value (a = 97, b = 98, etc)
+            // Subtract 97 from that. (a = 0, b = 1, etc)
+            // Use that value as an index for the accumulator array and set the array element to
+            // true
+            acc[c as usize - 97] = true;
 
-    // Get a lowercase version of the given sentence
-    let lowercase_sentence = sentence.to_lowercase();
-
-    // Start the count at zero
-    let mut count = 0;
-
-    // Iterate over all letters in the alphabet
-    for letter in alphabet.chars() {
-        // If the current letter is in the sentence...
-        if lowercase_sentence.contains(letter) {
-            // Add 1 to the count
-            count += 1
-        }
-    }
-
-    // If the count is 26 (the length of the alphabet), that means the sentence contains all letters
-    // and is a pangram.
-    count == alphabet.len()
+            // Return the accumulator array
+            acc
+        })
+        // Convert the array of true/false values into an iterator
+        .iter()
+        // Check if all values are true. If so, all letters are used in the sentence. Therefor, it
+        // is a pangram
+        .all(|&b| b)
 }
