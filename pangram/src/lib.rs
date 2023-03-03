628c416ee7f51866999516df6a291d7252c4ead6
Travis Horn <travis@travishorn.com>
2023-03-03 09:57:09 -0600

    Completed Pangram

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,4 +1,14 @@
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {sentence} a pangram?");
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let lowercase_sentence = sentence.to_lowercase();
+    let mut count = 0;
+
+    for letter in alphabet.chars() {
+        if lowercase_sentence.contains(letter) {
+            count += 1
+        }
+    }
+
+    count == alphabet.len()
 }
